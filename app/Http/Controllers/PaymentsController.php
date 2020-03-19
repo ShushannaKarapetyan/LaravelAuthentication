68391d3e9ee88453c2232c204927c8c94728690f
Shushanna <shushanna.karapetyan.97@gmail.com>
2020-03-19 14:21:46 +0400

    Eventing Pros and Cons

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\ProductPurchased;
 use App\Notifications\PaymentReceived;
 use Illuminate\Support\Facades\Notification;
 
@@ -18,5 +19,7 @@ class PaymentsController extends Controller
         # it's the same
 
         request()->user()->notify(new PaymentReceived(900));
+
+        ProductPurchased::dispatch('toy');
     }
 }
